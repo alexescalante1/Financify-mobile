12da0a21015b80937b075d29b4acd54ba2b3a25c
Orsted2 <alexescalante931@gmail.com>
2025-08-28 08:34:17 -0500

    holiwis 2

--- src/presentation/screens/Pages/beading/BeadingScreen.tsx
+++ src/presentation/screens/Pages/beading/BeadingScreen.tsx
@@ -3,7 +3,8 @@ import { View, ScrollView } from "react-native";
 import { Text } from "react-native-paper";
 
 export const BeadingScreen = () => (
-  <>
-  
-  </>
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text variant="headlineMedium">Cuentas</Text>
+    <Text variant="bodyLarge">Seguimiento de tus cuentas.</Text>
+  </View>
 );
